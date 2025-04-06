b4bc51b62917f2ca41dbdb4d4dc1220e7cb7f9d6
Cameron Jenkins <90398573+Ant1ing@users.noreply.github.com>
2025-04-06 15:20:05 +0100

    losing it now

--- src/components/CVDownload.jsx
+++ src/components/CVDownload.jsx
@@ -1,13 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const CVDownload = () => {
   return (
     <div className="cv-download">
         <img src="../images/text-wrap.png" alt="Rotating" className="rotating-image" />
-        <a href="../images/cameronjenkins.pdf" download="CameronJenkinsCV">
-            <FontAwesomeIcon icon={faDownload} size="1x" />
-        </a>
+        <a href="../images/cameronjenkins.pdf" download="CameronJenkinsCV"></a>
     </div>
   );
 };
